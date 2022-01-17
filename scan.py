0a49f5e11178e3e846a05d6c959204f97115a2a9
Parag <paragdurafe@outlook.com>
2022-01-17 23:15:30 +0530

    Video Capture

--- scan.py
+++ scan.py
@@ -1,1 +1,14 @@
 import cv2
+
+capture=cv2.VideoCapture(0)
+
+while True:
+    ret,frame=capture.read()
+
+    cv2.imshow('Video',frame)
+
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
