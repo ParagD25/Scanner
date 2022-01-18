7814f776ec397e8fa81a6730ea314cc1cdbce2cd
Parag <paragdurafe@outlook.com>
2022-01-18 23:50:22 +0530

    Detecting Paper Edges

--- scan.py
+++ scan.py
@@ -2,10 +2,22 @@ import cv2
 
 capture=cv2.VideoCapture(0)
 
+def basic(frame):
+
+    frameG=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    frameBlur=cv2.GaussianBlur(frameG,(7,7),1)
+    frameCanny=cv2.Canny(frameBlur,150,150)
+
+    return frameCanny
+
+
+
 while True:
     ret,frame=capture.read()
 
-    cv2.imshow('Video',frame)
+    myFrame=basic(frame)
+
+    cv2.imshow('Result',myFrame)
 
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
