83eb4c098e3b6fc837eaf1425fb4af4c867352e7
Parag <paragdurafe@outlook.com>
2022-01-21 23:55:55 +0530

    Finding Contours

--- scan.py
+++ scan.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 capture=cv2.VideoCapture(0)
 
@@ -8,12 +9,31 @@ def basic(frame):
     frameBlur=cv2.GaussianBlur(frameG,(7,7),1)
     frameCanny=cv2.Canny(frameBlur,150,150)
 
-    return frameCanny
+    kernel=np.ones((7,7))
 
+    frameDilate=cv2.dilate(frameCanny,kernel,iterations=2)
+    frameErode=cv2.erode(frameDilate,kernel,iterations=1)
+
+    return frameErode
+
+def getContours(img):
+    contours,hierarchy=cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area=cv2.contourArea(cnt)
+
+        if area>2500:
+            cv2.drawContours(frame2,cnt,-1,(255,0,0),3)
+
+            peri=cv2.arcLength(cnt,True)
+            approx=cv2.approxPolyDP(cnt,0.02*peri,True)
+            objcor=len(approx)
+
+            x,y,w,h=cv2.boundingRect(approx)
 
 
 while True:
     ret,frame=capture.read()
+    frame2=frame.copy()
 
     myFrame=basic(frame)
 
