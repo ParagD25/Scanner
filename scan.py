018456a05d28837a0dcec2e27707b6b18550e464
Parag <paragdurafe@outlook.com>
2022-01-23 23:30:02 +0530

    Correcting Warping Points

--- scan.py
+++ scan.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 
 capture=cv2.VideoCapture(0)
+width,height=capture.get(cv2.CAP_PROP_FRAME_WIDTH),capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print(width)
+print(height)
 
 def basic(frame):
 
@@ -36,15 +39,43 @@ def getContours(img):
 
     return largest
 
+def warpCorrected(points):
+    points=points.reshape(4,2)
+    newPoints=np.zeros((4,1,2),np.int32)
+
+    addPointVal=points.sum(1)
+
+    newPoints[0]=points[np.argmin(addPointVal)]
+    newPoints[-1]=points[np.argmax(addPointVal)]
+
+    diffPointVal=np.diff(points,axis=1)
+
+    newPoints[1]=points[np.argmin(diffPointVal)]
+    newPoints[-2]=points[np.argmin(diffPointVal)]
+
+    return newPoints
+
+def warp(frame,scanArea):
+    
+    point1=np.float32(scanArea)
+    point2=np.float32([[0,0],[capture.get(cv2.CAP_PROP_FRAME_WIDTH),0],[0,capture.get(cv2.CAP_PROP_FRAME_HEIGHT)],[capture.get(cv2.CAP_PROP_FRAME_WIDTH),capture.get(cv2.CAP_PROP_FRAME_HEIGHT)]])
+
+    matrix=cv2.getPerspectiveTransform(point1,point2)
+
+    imgScan=cv2.warpPerspective(frame,matrix,(capture.get(cv2.CAP_PROP_FRAME_WIDTH),capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+    return imgScan
+
 
 while True:
     ret,frame=capture.read()
     frame2=frame.copy()
 
     myFrame=basic(frame)
-    getContours(myFrame)
+    scanArea=getContours(myFrame)
+    # imgOutput=warp(frame,scanArea)
 
-    cv2.imshow('Result',frame2)
+    cv2.imshow('Result',scanArea)
 
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
