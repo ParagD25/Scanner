f34ddabefc2723c07d7ac84d24daabb2ebd263a9
Parag <paragdurafe@outlook.com>
2022-01-26 22:59:47 +0530

    Converting Images into PDF

--- pdfconverter.py
+++ pdfconverter.py
@@ -1,1 +1,17 @@
+import img2pdf
 from PIL import Image
+import os
+
+def makePDF():
+    os.chdir("Images")
+    images=os.listdir(".")
+    if len(images)==0:
+        print('No Scanned Images Present !!!')
+    else:
+        imgList=[x for x in images if x.endswith(".jpg")]
+
+        pdf=img2pdf.convert(imgList)
+
+        file=open("../PDF/Report.pdf","wb")
+        file.write(pdf)
+        file.close()
